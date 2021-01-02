c7ba747d0989a15e285e0693ecc367c81d135e3f
KirilPanika <pro-fighter@inbox.lv>
2021-01-02 20:27:54 +0200

    Add files via upload

---
+++ Labs/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    unsigned long long input, minus_input;
+    int count = 0;
+
+    cout << "Enter input ";
+    cin >> input;
+    minus_input = input;
+
+    while (minus_input % 10 != 1 && minus_input > 0){
+        minus_input /= 10;
+        count++;
+    }
+    if (minus_input % 10 == 1 ) {
+        minus_input /= 10;
+        while(count >= 0) {
+            minus_input *= 10;
+            count--;
+        }
+        cout << "Result is " << input - minus_input << endl;
+    } else{
+        cout << "404" << endl;
+    }
+    return 0;
+}
