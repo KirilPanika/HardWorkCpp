360b0926ddd21d593e643f3a0a0ca090d48b96b5
KirilPanika <pro-fighter@inbox.lv>
2021-01-02 19:56:04 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a;
+    cout << " Enter the number A: ";
+    cin >> a;
+
+    int b;
+    cout << "Enter the number B: ";
+    cin >> b;
+
+    int c;
+    cout << "Enter the number C: ";
+    cin >> c;
+
+    if (a > b && a < c || a > c && a < b) {
+        cout << "The second biggest number is A: " << a;
+    }
+    if (b > a && b < c || b > c && b < a) {
+        cout << "The second biggest number is B: " << b;
+    }
+    if (c > a && c < b || c > b && c < a) {
+        cout << "The second biggest number is C: " << c;
+    }
+    if (a == b && a < c) {
+        cout << "The second biggest number is B: " << b;
+    }
+    if (b == c && b < a) {
+        cout << "The second biggest number is B: " << b;
+    }
+    if (a == c && b > a) {
+        cout << "The second biggest number is C: " << c;
+    }
+    if (a == b && a == c) {
+        cout << "The second biggest number is B: " << b << ", but they are the same!";
+    }
+
+    return 0;
+}
