bceb6a632c7f92979bd9f75a2be0167ae727e61a
KirilPanika <pro-fighter@inbox.lv>
2021-01-02 19:55:26 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    //inputing number
+    float number;
+    cout << "Enter the number: ";
+    cin >> number;
+
+    cout << roundf(number);
+
+    
+
+    return 0;
+}
