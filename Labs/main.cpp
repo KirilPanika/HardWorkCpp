740d0c39b32e47f0cf8b354fd857564b397e8e33
KirilPanika <pro-fighter@inbox.lv>
2021-01-02 20:30:44 +0200

    Add files via upload

---
+++ Labs/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter size of array:";
+    cin >> n;
+    double arr[n][n];
+    int b = n - 1;
+    double sum = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            arr[i][j] = (double) (rand()%10+1);
+            printf("%4g ", arr[i][j]);
+        }
+        cout << endl;
+        sum += arr[i][b];
+        b -= 1;
+    }
+    cout << sum / n;
+
+
+
+
+    return 0;
+}
