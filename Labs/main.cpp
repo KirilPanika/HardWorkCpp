cd1d47f3c0953259f391ee34078e9a7419f03b79
KirilPanika <pro-fighter@inbox.lv>
2021-01-02 20:29:14 +0200

    Add files via upload

---
+++ Labs/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // inputting n
+    int n;
+    cout << "Enter n:";
+    cin >> n;
+
+    // inputting array
+    int arr[n];
+    cout << "Enter elements for array: "<<endl;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    // printing array
+    for(int i=0; i<n; i++){
+        cout << arr[i] << ' ';
+    }
+
+    // finding max
+    int max = arr[0];
+    int placemax = 0;
+    for(int i=0; i<n; i++){
+        if(max < arr[i]){
+            max = arr[i];
+            placemax = i;
+        }
+    }
+
+    // counting average
+    float sum = 0;
+    for(int i=0; i<placemax; i++){
+        sum += arr[i];
+    }
+
+    if (placemax != 0){
+        cout << endl << "Result: " << sum / placemax;
+    } else {
+        cout << endl << "There is no elements to calculate average from";
+    }
+    return 0;
+}
