59650f27d67369fcb251d1b0e74eef5391ac6231
KirilPanika <pro-fighter@inbox.lv>
2021-01-05 11:36:58 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+//vvoditsja 10 4isel v cikle, proveritj v vozrastausem li oni porjadke
+int main() {
+
+    int a;
+    cout << "Enter the numbers:";
+
+    while (cin >> a) {
+        if () {
+
+        }
+    }
+
+    return 0;
+}
