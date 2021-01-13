fa0944c626077b5b3a7949ecdd6d00429e26dbd4
KirilPanika <pro-fighter@inbox.lv>
2021-01-13 11:25:13 +0200

    Update hmw III no2 arr.cpp

--- hmw III no2 arr.cpp
+++ hmw III no2 arr.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // dan massiv. Peredvinut vse schisla na odnu poziciju
 int main() {
 
-
+    /*
     int arr[5] = {1, 2, 3, 4, 5};
     int buf = arr[0];
 
@@ -13,6 +13,23 @@ int main() {
     for (int i = 1; i < 5; i++) {
         cout << arr[i] << ", ";
     }   cout << buf;
+    */
+    
+    
+    /*
+    int arr[5] = {1, 2, 3, 4, 5};
+    int buf = arr[0], lol = arr[1], kek = arr[2], rock = arr[3], cat = arr[4];
 
+    for (int i = 0; i < 5; i++) {
+     arr[0] = cat;
+     arr[1] = buf;
+     arr[2] = lol;
+     arr[3] = kek;
+     arr[4] = rock;
+     cout << arr[i] << " ";
+    }
+    */
+    
+    
     return 0;
 }
