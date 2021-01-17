719a7b64e0f6f65e4198c0a9e99a974376652465
KirilPanika <pro-fighter@inbox.lv>
2021-01-17 15:10:01 +0200

    Update hmw III no2 arr.cpp

--- hmw III no2 arr.cpp
+++ hmw III no2 arr.cpp
@@ -31,5 +31,18 @@ int main() {
     */
     
     
+    int arr[5] = {1, 2, 3, 4, 5};
+    int buf = arr[4];
+    
+    for (int i = 4; i > 0; i--) {
+     arr[i] = arr[i-1];
+    }
+    arr[0] = buf;
+
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    
     return 0;
 }
