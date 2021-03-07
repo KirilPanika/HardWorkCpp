8cba34a39d411a9389409029f94c2a469b832537
KirilPanika <pro-fighter@inbox.lv>
2021-03-07 10:49:57 +0200

    Create str2.cpp

---
+++ str2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+        // get all 'a' letters
+int main() {
+  
+  
+  string s;
+  cin >> s;
+  
+  string res = "";
+  
+  for (int i = 0; i < s.length(); i++) {
+      if (tolower(s[i]) == 'a') {
+          res += s[i];
+      }
+  } cout << res;
+  
+
+
+    return 0;
+}
