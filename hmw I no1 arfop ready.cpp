ffd30c2e03b2542fbf8f6201605d5ffabb87d47b
KirilPanika <pro-fighter@inbox.lv>
2021-03-29 18:18:48 +0200

    Update hmw I no1 arfop ready.cpp

--- hmw I no1 arfop ready.cpp
+++ hmw I no1 arfop ready.cpp
@@ -5,16 +5,16 @@ using namespace std;
 // prikleitj v konce 4islo 6
 int main () {
 
-  int var;
-  cout << "Enter the number:";
-  cin >> var;
+  int var;                             // new variable
+  cout << "Enter the number:";         // to show in console sentence
+  cin >> var;                          // to write some number to variable
   
-  int sum;
-  if (var >= 0) {
-    sum = var * 10 + 6;
-    cout << sum;
-  } else {
-    cout << "Error";
+  int sum;                             // new variable for result
+  if (var >= 0) {                      // if it's true do next line
+    sum = var * 10 + 6;                // to do and write the result to sum variable
+    cout << sum;                       // to show the variable sum
+  } else {                             // else if it's not true do next line
+    cout << "Error";                   // 
   }
 
   return 0;
