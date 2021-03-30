4a8ebd3727242c2cd6018d332563b3bc547cc8d1
KirilPanika <pro-fighter@inbox.lv>
2021-03-30 19:22:43 +0200

    Update hmw I no3 arfop ready.cpp

--- hmw I no3 arfop ready.cpp
+++ hmw I no3 arfop ready.cpp
@@ -5,19 +5,19 @@ using namespace std;
 // perevesti 4islo v days:hours:minutes:seconds
 int main() {
     // I задание 3
-    int var;
-    cout << "Enter the number: "<< endl;
-    cin >> var;
+    int var;                                                            // new variable (seconds)
+    cout << "Enter the number: "<< endl;                                // to show the sentence in console
+    cin >> var;                                                         // to write down the number to the variable
 
-    int day = var / (24 * 3600);
-    var = var % (24 * 3600);
-    int hour = var / 3600;
-    var %= 3600;
-    int minutes = var / 60 ;
-    var %= 60;
-    int seconds = var;
+    int day = var / (24 * 3600);                                        // new variable day = formula how to find days 
+    var = var % (24 * 3600);                                            // new variable with seconds (seconds - days)
+    int hour = var / 3600;                                              // new variable hour = formula how to find hours
+    var %= 3600;                                                        // new variable with seconds (seconds - hours)
+    int minutes = var / 60 ;                                            // new variable minutes = formula how to find minutes
+    var %= 60;                                                          // new variable with seconds (seconds - last seconds)
+    int seconds = var;                                                  // new variable with final seconds
 
-    cout << day << ":" << hour << ":" << minutes << ":" << seconds;
+    cout << day << ":" << hour << ":" << minutes << ":" << seconds;     // to show in console the results
 
     return 0;
 }
