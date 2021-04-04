6e6d8b8b0b760fdf97b88e225e3cd6f6218d1068
KirilPanika <pro-fighter@inbox.lv>
2021-04-04 19:02:42 +0200

    Update str4.cpp

--- str4.cpp
+++ str4.cpp
@@ -3,17 +3,17 @@
 
 using namespace std;
 
-        // get all vowels
+        // get all vowels (Гласные)
 int main() {
   
-  string vowels = "aeiou";
+  string vowels = "aeiou";                              // new string variable
   string s;
   cin >> s;
   
-  string res = "";
+  string res = "";                                      // new string variable and there are nothing
   
   for (int i = 0; i < s.length(); i++) {
-    if (vowels.find( tolower(s[i]) ) != -1) {
+    if (vowels.find( tolower(s[i]) ) != -1) {           // 
         res += s[i];
     }
      
